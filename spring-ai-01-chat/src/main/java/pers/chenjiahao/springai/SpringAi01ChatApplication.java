e415ce065f1821321da2ee3b02816114817e6109
chenjiahao <834713285@qq.com>
2024-07-28 22:48:22 +0800

    init

---
+++ spring-ai-01-chat/src/main/java/pers/chenjiahao/springai/SpringAi01ChatApplication.java
@@ -0,0 +1,13 @@
+package pers.chenjiahao.springai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAi01ChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAi01ChatApplication.class, args);
+    }
+
+}
---
+++ spring-ai-01-chat/src/test/java/pers/chenjiahao/springai/SpringAi01ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.chenjiahao.springai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAi01ChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
